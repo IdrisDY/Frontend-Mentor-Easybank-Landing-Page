d5914f8f78910877009f2db3fdccb09215689a9c
Idris adekunle <idrisadekunle726@gmail.com>
2022-06-14 20:32:38 +0100

    desktop and mobile intro done

--- src/Components/Landing-Content.js
+++ src/Components/Landing-Content.js
@@ -3,13 +3,26 @@ import Mockup from './images/image-mockups.png'
 
 const LandingContent = () => {
   return (
-    <div>
-    <div className='mockups' >
+    <section className='mockups'>
+    <div className='mockup-div' >
 
-<img src={Mockup} alt ='#'/>
+<img src={Mockup} alt ='#' className='mockup-img' />
+    </div>
+
+    <div className='intro-div'>
+    <div className = 'intro-content'>
+    <h1 class ='head-text'>   Next generation digital banking</h1>
+<p className='intro-text-body'>   Take your financial life online. Your Easybank account will be a one-stop-shop 
+  for spending, saving, budgeting, investing, and much more.
+</p>
+<button className='req-btn'> Request Invite</button>
+
+
     </div>
 
     </div>
+
+    </section>
   )
 }
 
