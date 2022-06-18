ed82508de4834fb9c63cbbb5b38dcb968fb36edf
Idris adekunle <idrisadekunle726@gmail.com>
2022-06-18 14:12:22 +0100

    added interactive button,links,styled cards

--- src/Components/Latest.js
+++ src/Components/Latest.js
@@ -10,32 +10,32 @@ const Latest= ()=>{
          <div className="cast"><h1 className="arti">Latest Articles</h1>
 
          <div className="card">
-           <div className="cards"><div  className="pic"><img className="image" src={money}/></div>
-           <div  className="white"><br/><p >By Claire Robinson</p><br/>
-  <h3>Receive money in any<br/> currency with no fees</h3><br/>
- <p className="parag">The world is getting smaller and<br/> we’re becoming more mobile. So<br/> why should you be 
-  forced to only<br/> receive money in a single …</p></div></div>
+           <div className="cards"><div  className="pic"><img className="image" id = "picture1" src={money}/></div>
+           <div  className="white"><p className="parag" >By Claire Robinson</p>
+  <h3 className="h3-new-font">Receive money in any currency with no fees</h3>
+ <p className="parag">The world is getting smaller and we’re becoming more mobile. So why should you be 
+  forced to only receive money in a single …</p></div></div>
 
 
            <div className="cards"><div  className="pic"><img className="image" src={restaurant}/></div>
-           <div  className="white"><br/><p>By Wilson Hutton</p><br/>
-  <h3>Treat yourself without<br/> worrying about money</h3><br/>
-  <p className="parag">Our simple budgeting feature<br/> allows you to separate out your<br/> spending and set 
-  realistic limits<br/> each month. That means you …</p></div></div>
+           <div  className="white"><p className="parag">By Wilson Hutton</p>
+  <h3 className="h3-new-font">Treat yourself without worrying about money</h3>
+  <p className="parag">Our simple budgeting and feature allows you to separate out your spending and set 
+  realistic limits each month. That means you …</p></div></div>
 
 
            <div className="cards"><div  className="pic"><img className="image" src={plane}/></div>
-           <div  className="white"><br/><p>By Wilson Hutton</p><br/>
-  <h3>Take your Easybank card<br/> wherever you go</h3><br/>
- <p className="parag"> We want you to enjoy your travels.<br/> This is why we don’t charge any<br/> fees on purchases 
-  while you’re<br/> abroad. We’ll even show you …</p></div></div>
+           <div  className="white"><p className="parag">By Wilson Hutton</p>
+  <h3 className="h3-new-font">Take your Easybank card wherever you go</h3>
+ <p className="parag"> We want you to enjoy your travels. This is why we don’t charge any fees on purchases 
+  while you’re abroad. We’ll even show you …</p></div></div>
 
 
            <div className="cards"><div  className="pic"><img className="image" src={confetti}/></div>
-           <div  className="white"><br/><p> By Claire Robinson</p><br/>
-  <h3>Our invite-only Beta<br/> accounts are now live!</h3><br/>
-  <p className="parag">After a lot of hard work by the<br/> whole team, we’re excited to launch<br/> our closed beta. 
-  It’s easy to request<br/> an invite through the site ...</p></div></div>
+           <div  className="white"><p className="parag"> By Claire Robinson</p>
+  <h3 className="h3-new-font">Our invite-only Beta accounts are now live!</h3>
+  <p className="parag">After a lot of hard work by the whole team, we’re excited to launch our closed beta. 
+  It’s easy to request an invite through the site ...</p></div></div>
          </div>
          
          
